b87ca109a4a5cfcec12241f12fefd4adb958e322
Calie Rushton <calielrushton@gmail.com>
2019-01-12 19:08:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,11 +35,11 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  occupation = [] # will hold the values of the occupation key for people whose location matches hometown
+  occupation = [] 
   data.each do |series, attributes|
     attributes.each do |key, value|
       if key["hometown"] == hometown
-        occupation.push key["occupation"]
+        occupation.push key["occupation"] # value of occupation key
       end
     end
   end
@@ -51,8 +51,9 @@ def get_average_age_for_season(data, season)
   data.each do |series, attributes|
     if series == season # identify correct season
       attributes.each do |key, value|
-
+        ages.push key["age"].to_i
       end
     end
   end
+  ages
 end
